e6d782b4b7f454381414188a4afc6a1d567954a6
Shaik <shaikshabzan@gmail.com>
2019-01-25 12:33:41 +0530

    adds circuit breaker

---
+++ circuit-website/src/main/java/com/cg/breaker/circuitwebsite/CircuitWebsiteApplication.java
@@ -0,0 +1,14 @@
+package com.cg.breaker.circuitwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitWebsiteApplication.class, args);
+	}
+
+}
+
